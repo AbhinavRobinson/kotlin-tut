d9d30c52f2150ef5ea8357a5e06442089e02e12f
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-15 22:46:32 +0530

    (feat): lists!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,19 +2,35 @@ fun main() {
     // val is constant
     val x = 10
     println("X = $x")
+
     // var is variable and F means its a Float type
-    var y = 2F
-    y = x.toFloat() / 2
+    val y: Float = x.toFloat() / 2
     println("Y = X = $y")
+
     // Different types can divide
     val z = y/x
     println("Z = $z")
+
     println("STARS: ")
-    val input: Int = (readLine() ?: "0").toInt()
-    for (i in 0..input) {
+    // Type safety + give default value if null, NULL safety
+    // val input: Int = (readLine() ?: "0").toInt()
+    // for loops :)
+    for (i in 0..3) {
         for (j in 0..i) {
             print("*")
         }
         println()
+    }
+
+    // Lists
+    val shoppingList = mutableListOf("Laptop", "Mobile", "PC")
+    shoppingList.add("Watch")
+    var index = 0
+    while(index < shoppingList.size) {
+        print(shoppingList[index]+",")
+        index++
+    }
+    for (i in shoppingList) {
+        println(i)
     }
 }
