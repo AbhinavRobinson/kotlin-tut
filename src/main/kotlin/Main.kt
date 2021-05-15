6a9cb946781602fc4bbf0ee103047464be011834
AbhinavRobinson <abhinavrobinson@gmail.com>
2021-05-15 22:22:23 +0530

    (feat): maths!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,12 @@
 fun main() {
-    val x: Int = 10
-    println(x)
+    // val is constant
+    val x = 10
+    println("X = $x")
+    // var is variable and F means its a Float type
+    var y = 2F
+    y = x.toFloat() / 2
+    println("Y = X = $y")
+    // Different types can divide
+    val z = y/x
+    println("Z = $z")
 }
